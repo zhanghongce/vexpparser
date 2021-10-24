0aeb63535e0a8d9a60d696881cafc99cdc195c30
Hongce Zhang <zhanghongce@126.com>
2021-10-24 08:11:08 +0800

    add check

--- src/vexp.cc
+++ src/vexp.cc
@@ -7,6 +7,7 @@ namespace verilog_expr {
 
 // warning : will not make copy of annotation!
 VExprAst::VExprAstPtr VExprAst::MakeCopyWithNewChild(const VExprAstPtrVec& in) const {
+  assert(in.size() == child_.size());
   auto ret = std::make_shared<VExprAst>(op_ , in, parameter_, str_parameter_);
   return ret;
 }
